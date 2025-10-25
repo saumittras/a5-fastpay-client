de7a12139da8cb4c08cec8f3b9235f4b4cc70e67
Saumittra Sarker <saumittra.eee@gmail.com>
2025-10-25 20:53:02 +0600

    added basic setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello from here</h1>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
